9cc7c597facd30b1d8ec3202f75100371b3cff54
pprginy <2390432934@qq.com>
2017-08-17 17:12:59 +0800

    test1

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hellasdasdo Servlet!</h1>");
+            response.getWriter().println("<h1>Hsbbo Servlet!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
 
