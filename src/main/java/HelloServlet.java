04c00b1b2fd5ddc563a0f9388c39f37c422b38b6
pprginy <2390432934@qq.com>
2017-08-17 17:08:16 +0800

    test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+@WebServlet(name = "HelloServlet")
+public class HelloServlet extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+}
